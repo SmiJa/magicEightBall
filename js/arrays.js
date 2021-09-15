35d2810fffd9b5762c19cbb944f695415c74bdf0
Jason Smith <SmiJa@users.noreply.github.com>
2021-09-14 22:32:02 -0700

    Have the main logo on the page with the background working.

---
+++ js/arrays.js
@@ -0,0 +1,34 @@
+const affirmative = [
+    'It is certain.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes definitely',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most likely',
+    'Outlook good.',
+    'Yes.',
+    'Signs point to yes.'
+];
+
+const nonCommittal = [
+    'Reply hazy, try again.',
+    'Ask again later.',
+    'Better not tell you now',
+    'Cannot predict now.',
+    'Concentrate and ask again.'
+];
+
+const negative = [
+    "Don't count on it.",
+    'My replay is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Very doubtful.'
+];
+
+const types = [
+    affirmative,
+    nonCommittal,
+    negative
+];
---
+++ js/functions.js
@@ -0,0 +1,21 @@
+function randNum(num) {
+    let selection = Math.floor((Math.random() * num));
+    // console.log(`Random num: ${selection}`);
+    return selection;
+}
+
+function getAnswer(num){
+    let selection = randNum(num);
+    return types[selection][randNum(types[selection].length)];
+}
+
+function askQuestion() {
+    let answer = getAnswer(types.length);
+    console.clear();
+
+    if (questionInput.value === "") {
+        alert("Please be sure to ask a question.");
+    }
+    // console.log(types[0][4]);
+    return ballWrap.innerText = answer;
+}
